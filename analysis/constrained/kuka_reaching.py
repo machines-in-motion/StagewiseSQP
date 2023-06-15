5adf3f34bd72bd6c70ee06e99367896aec0e11b7
Sébastien Kleff <sk8001@nyu.edu>
2023-06-15 17:13:58 -0400

    started to implement cone in lwa for ocp example in paper

--- analysis/constrained/kuka_reaching.py
+++ analysis/constrained/kuka_reaching.py
@@ -9,9 +9,7 @@ import crocoddyl
 import numpy as np
 import pinocchio as pin
 np.set_printoptions(precision=4, linewidth=180)
-import ocp_utils
-from sqp_ocp.constraint_model import StateConstraintModel, ControlConstraintModel, EndEffConstraintModel, NoConstraintModel, ConstraintModelStack
-from sqp_ocp.solvers import SQPOCP
+from friction_cone import FrictionConeConstraint
 
 
 # # # # # # # # # # # # #
@@ -71,7 +69,7 @@ running_DAM = crocoddyl.DifferentialActionModelContactFwdDynamics(state, actuati
 terminal_DAM = crocoddyl.DifferentialActionModelContactFwdDynamics(state, actuation, crocoddyl.ContactModelMultiple(state, actuation.nu), terminalCostModel)
 
 # Create Integrated Action Model (IAM), i.e. Euler integration of continuous dynamics and cost
-dt = 1e-2
+dt = 1e-3
 runningModel = crocoddyl.IntegratedActionModelEuler(running_DAM, dt)
 terminalModel = crocoddyl.IntegratedActionModelEuler(terminal_DAM, 0.)
 
@@ -80,7 +78,7 @@ terminalModel = crocoddyl.IntegratedActionModelEuler(terminal_DAM, 0.)
 # terminalModel.differential.armature = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.])
 
 # Create the shooting problem
-T = 10
+T = 100
 problem = crocoddyl.ShootingProblem(x0, [runningModel] * T, terminalModel)
 
 
@@ -88,40 +86,67 @@ problem = crocoddyl.ShootingProblem(x0, [runningModel] * T, terminalModel)
 # choose scenario: 0 or 1 or 2 or 3
 option = 0
 
+# State (joint all vel box) and control constraints (torque axis 2)
 if option == 0:    
   clip_state_max = np.array([np.inf]*14)
-  clip_state_min = -np.array([np.inf]*7 + [0.5]*7)
-  clip_ctrl = np.array([np.inf, 40 , np.inf, np.inf, np.inf, np.inf , np.inf] )
-
-
+  clip_state_min = -np.array([np.inf]*7 + [np.inf]*7)
+  clip_ctrl = np.array([np.inf, np.inf , np.inf, np.inf, np.inf, np.inf , np.inf] )
   statemodel = crocoddyl.StateConstraintModel(state, 7, clip_state_min, clip_state_max, 'stateConstraint')
   controlmodel = crocoddyl.ControlConstraintModel(state, 7,  -clip_ctrl, clip_ctrl, 'ctrlConstraint')
-
   nc = statemodel.nc + controlmodel.nc
   ConstraintModel = crocoddyl.ConstraintStack([statemodel, controlmodel], state, nc, 7, 'runningConstraint')
-  clip_state_end = np.array([np.inf, np.inf, np.inf, np.inf, np.inf, np.inf , np.inf] + [0.01]*7)
-  terminal = crocoddyl.StateConstraintModel(state, 7, clip_state_min, clip_state_max, 'stateConstraint')
+  clip_state_end = np.array([np.inf, np.inf, np.inf, np.inf, np.inf, np.inf , np.inf] + [0.0]*7)
+  terminal = crocoddyl.StateConstraintModel(state, 7, -clip_state_end, clip_state_end, 'stateConstraint')
   TerminalConstraintModel = crocoddyl.ConstraintStack([terminal], state, 14, 7, 'terminalConstraint')
-
   constraintModels =[controlmodel] * (T) + [terminal]
+
+  # # Plot results 
+  # p_mea1 = get_p_(r1.data['joint_positions'][N_start:N], pinrobot.model, pinrobot.model.getFrameId('contact'))
+  # p_mea2 = get_p_(r2.data['joint_positions'][N_start:N], pinrobot.model, pinrobot.model.getFrameId('contact'))
+  # fig_circle, ax_circle = plot_endeff_yz(p_mea2, target_position, "Constrained") 
+  # ax_circle.plot(p_mea1[:,1], p_mea1[:,2], color='g', linewidth=4, label='Unconstrained', alpha=0.5) 
+  # ax_circle.set_xlim(-0.33, +0.33)
+  # ax_circle.set_ylim(0.15, 0.8)
+  # ax_circle.plot(p_mea2[0,1], p_mea2[0,2], 'ro', markersize=16)
+  # ax_circle.text(0., 0.1, '$x_0$', fontdict={'size':26})
+  # handles, labels = ax_circle.get_legend_handles_labels()
+  # fig_circle.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.12, 0.885), prop={'size': 26}) 
+  # fig_circle.savefig('/home/skleff/data_paper_fadmm/jointpos_circle_plot.pdf', bbox_inches="tight")
+  # # Joint pos
+
+# Only state constraints
 elif option == 1:    
   clip_state_max = np.array([np.inf]*14)
   clip_state_min = -np.array([np.inf]*7 + [0.5]*7)
-  statemodel = StateConstraintModel(state, 7, clip_state_min, clip_state_max)
+  statemodel = crocoddyl.StateConstraintModel(state, 7, clip_state_min, clip_state_max, 'stateConstraint')
   clip_state_end = np.array([np.inf, np.inf, np.inf, np.inf, np.inf, np.inf , np.inf] + [0.00]*7)
-  TerminalConstraintModel = StateConstraintModel(state, 7, -clip_state_end, clip_state_end)
-  constraintModels =  [NoConstraintModel(state, 7)] + [statemodel] * (T-1) + [TerminalConstraintModel]
+  TerminalConstraintModel = crocoddyl.StateConstraintModel(state, 7, -clip_state_end, clip_state_end, 'stateConstraint')
+  constraintModels =  [crocoddyl.NoConstraintModel(state, 7)] + [statemodel] * (T-1) + [TerminalConstraintModel]
 
+# End-effector constraints
 elif option == 2:
   endeff_translation = np.array([0.7, 0, 1.1]) # move endeff +30 cm along x in WORLD frame
-
   lmin = np.array([-np.inf, endeff_translation[1], endeff_translation[2]])
   lmax =  np.array([np.inf, endeff_translation[1], endeff_translation[2]])
-  constraintModels = [NoConstraintModel(state, 7)] + [EndEffConstraintModel(state, 7, fid, lmin, lmax)] * T
+  constraintModels = [crocoddyl.NoConstraintModel(state, 7)] + [crocoddyl.FrameTranslationConstraintModel(state, 7, fid, lmin, lmax)] * T
 
+# # Force constraint
+# elif option == 3:
+#   clip_force_min = np.array([np.inf]*3)
+#   clip_force_max = -np.array([np.inf, np.inf, 30.])
+#   clip_ctrl = np.array([np.inf, 40 , np.inf, np.inf, np.inf, np.inf , np.inf] )
+#   clip_state_max = np.array([np.inf]*14)
+#   clip_state_min = -np.array([np.inf]*14)
+#   problem.runningModels[0].differential.
+#   statemodel = crocoddyl.StateConstraintModel(state, 7, clip_state_min, clip_state_max, 'stateConstraint')
+#   controlmodel = crocoddyl.ControlConstraintModel(state, 7,  -clip_ctrl, clip_ctrl, 'ctrlConstraint')
+#   forcemodel = crocoddyl.ContactForceConstraintModel3D(state, actuation.nu, fid, clip_force_min, clip_force_max, "forceConstraint", pin.LOCAL_WORLD_ALIGNED)
+#   constraintModel = crocoddyl.ConstraintStack([forcemodel, statemodel], state, statemodel.nc + forcemodel.nc, 7, 'runningConstraint')
+#   constraintModels =  [constraintModel]*T + [constraintModel] #+ [forcemodel] * (T-1) + [statemodel]
 
-elif option == 3:
-  constraintModels = [NoConstraintModel(state, 7)] * (T+1)
+# No constraints
+elif option == 4:
+  constraintModels = [crocoddyl.NoConstraintModel(state, 7)] * (T+1)
 
 
 xs = [x0] * (T+1)
@@ -130,70 +155,85 @@ us = [np.zeros(nu)] * T
 
 
 ddp = crocoddyl.SolverFADMM(problem, constraintModels)
-
-
 # ddp = crocoddyl.SolverPROXQP(problem, constraintModels)
-
 # ddp = crocoddyl.SolverGNMS(problem)
-
 # ddp = SQPOCP(problem, constraintModels, "ProxQP")
 # ddp = SQPOCP(problem, constraintModels, "OSQP")
-qp_iters = 1000
-sqp_ites = 100
+qp_iters = 10000
+sqp_ites = 500
 ddp.with_callbacks = True
 ddp.use_filter_ls = True
-ddp.filter_size = 10
-ddp.termination_tol = 1e-3
+ddp.filter_size = 100
+ddp.termination_tol = 1e-6
 ddp.warm_start = True
 ddp.max_qp_iters = qp_iters
-
 # ddp.calc()
 import time
-t1 = time.time()
+# t1 = time.time()
 ddp.solve(xs, us, sqp_ites)
-t2 = time.time()
-print(t2 - t1)
-
-print("after warm start")
-ddp.solve(ddp.xs, ddp.us, sqp_ites)
-
-
-# xs = [x0] * (T+1)
-# us = [np.zeros(nu)] * T 
-
-
-# problem = crocoddyl.ShootingProblem(x0, [runningModel] * T, terminalModel)
-
-# ddppy = SQPOCP(problem, constraintModels, "FADMM")
-# # ddppy = crocoddyl.SolverGNMS(problem)
-
-# ddppy.verbose = True
-# ddppy.verboseQP = False
-# ddppy.max_iters = qp_iters
-
-# ddppy.solve(xs, us, sqp_ites)
-
-# # print(np.array(ddp.dx_tilde)[1], ddppy.dx_tilde[1])
-# # print(np.linalg.norm(np.array(ddp.dx_tilde) - ddppy.dx_tilde))
-# # print(np.linalg.norm(np.array(ddp.du_tilde) - ddppy.du_tilde))
-# # print("_____________________________________________")
-# # for i in range(T+1):
-# #   print(np.linalg.norm(ddp.Vx[i] - ddppy.s[i]))
-# #   print(np.linalg.norm(ddp.Vxx[i] - ddppy.S[i]))
-
-# # for i in range(len(ddppy.lag_mul)):
-# #   print(np.linalg.norm((ddp.lag_mul)[i] - ddppy.lag_mul[i]))
-# #   # print(np.linalg.norm((ddp.y)[i] - ddppy.y[i]))
-# #   # print(np.linalg.norm((ddp.get_rho_vec)[i] - ddppy.rho_vec[i]))
-# #   # print(np.linalg.norm((ddp.z)[i] - ddppy.z[i]))
-# #   print("________________________________________")
-# print(np.linalg.norm(np.array(ddp.xs) - ddppy.xs)/(T+1))
-# print(np.linalg.norm(np.array(ddp.us) - ddppy.us)/T)
+# t2 = time.time()
+# print(t2 - t1)
+# print("after warm start")
+# ddp.solve(ddp.xs, ddp.us, sqp_ites)
 
 
 
-# # Extract DDP data and plot
-# # ddp_data = ocp_utils.extract_ocp_data(ddp, ee_frame_name='contact')
+# Generate plot of number of iterations for each problem
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams["pdf.fonttype"] = 42
+matplotlib.rcParams["ps.fonttype"] = 42
+ 
 
-# # ocp_utils.plot_ocp_results(ddp_data, which_plots="all", labels=None, markers=['.'], colors=['b'], sampling_plot=1, SHOW=True)
+# Plot end-effector trajectory (y,z) plane
+# 
+def plot_joint_velocity(ddpSolver, fig=None, ax=None, label=None):
+  x = np.array(ddpSolver.xs)
+  q = x[:,nq:] 
+  # Plots
+  tspan = np.linspace(0, T*dt, T+1)
+  if(ax is None or fig is None):
+      fig, ax = plt.subplots(nq, 1, sharex='col') 
+  if(label is None):
+      label='Position'
+  for i in range(nq):
+      # Plot positions
+      ax[i].plot(tspan, q[:,i], color='b', linewidth=4, label=None, alpha=0.5)
+      ax[i].set_ylabel('$v_%s$'%i, fontsize=20)
+      # ax[i].set_xlim(, +0.33)
+      ax[i].set_ylim(-model.velocityLimit[i],+model.velocityLimit[i])
+      # ax[i].yaxis.set_major_locator(plt.MaxNLocator(2))
+      # ax[i].yaxis.set_major_formatter(plt.FormatStrFormatter('%.2e'))
+      ax[i].grid(True)
+      ax[i].tick_params(axis = 'y', labelsize=18)
+      ax[i].tick_params(axis = 'x', labelsize=18)
+  # Common x-labels + align
+  ax[-1].set_xlabel('Time (s)', fontsize=16)
 
+  ax[-1].grid(True) 
+  # y axis labels
+  # fig.text(0.05, 0.5, 'Joint position (rad)', va='center', rotation='vertical', fontsize=16)
+  # fig.text(0.49, 0.5, 'Joint velocity (rad/s)', va='center', rotation='vertical', fontsize=16)
+  # fig.text(0.05, 0.5, 'Joint velocity (rad/s)', va='center', rotation='vertical', fontsize=16)
+  fig.subplots_adjust(wspace=0.27)
+  fig.align_ylabels()
+
+  handles, labels = ax[0].get_legend_handles_labels()
+  fig.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.12, 0.885), prop={'size': 26}) 
+  fig.savefig('/home/skleff/data_paper_fadmm/ocp_case1.pdf', bbox_inches="tight")
+  return fig, ax 
+
+plot_joint_velocity(ddp)
+# def plot_state(jmea, label):
+    # fig0, ax0 = plt.subplots(1, 1, figsize=(19.2,10.8))
+    # # Measured
+    # ax0.plot(xdata, jmea, color='b', linewidth=4, label=label, alpha=0.5) 
+    # # Axis label & ticks
+    # ax0.set_ylabel('Joint position $q_1$ (rad)', fontsize=26)
+    # ax0.set_xlabel('Time (s)', fontsize=26)
+    # ax0.tick_params(axis = 'y', labelsize=22)
+    # ax0.tick_params(axis = 'x', labelsize=22)
+    # ax0.grid(True) 
+    # return fig0, ax0
+plt.show()
+plt.close('all')
