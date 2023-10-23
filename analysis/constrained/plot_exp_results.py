c282111da7a7252eee29f44355bed11f51b3a289
Sebastien Kleff <sk8001@nyu.edu>
2023-10-23 19:06:04 -0400

    updated paper figures

--- analysis/constrained/plot_exp_results.py
+++ analysis/constrained/plot_exp_results.py
@@ -3,6 +3,13 @@ from croco_mpc_utils.pinocchio_utils import *
 import numpy as np
 from robot_properties_kuka.config import IiwaConfig
 
+# Generate plot of number of iterations for each problem
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams["pdf.fonttype"] = 42
+matplotlib.rcParams["ps.fonttype"] = 42
+ 
+ 
 KUKA_DGH_PATH   = os.path.join(os.path.dirname(__file__), '../../kuka_dgh')
 os.sys.path.insert(1, str(KUKA_DGH_PATH))
 
@@ -27,8 +34,11 @@ config        = launch_utils.load_config_file(EXP_NAME, path_prefix=KUKA_DGH_PAT
 PLOTS = [
             'circle_ssqp',
             'circle_cssqp_joint',
+            
             # 'circle_cssqp_ee',
+            
             # 'square_cssqp',
+            
             # 'line_cssqp',
             # 'plane_cssqp'
         ]
@@ -114,22 +124,17 @@ N_START         = int(T_START * config['ctrl_freq'])
 xdata           = np.linspace(0, (N-N_START)/config['ctrl_freq'], N-N_START) 
 
 
-# Generate plot of number of iterations for each problem
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.rcParams["pdf.fonttype"] = 42
-matplotlib.rcParams["ps.fonttype"] = 42
- 
+LINEWIDTH = 8
+
 # Plot end-effector trajectory (y,z) plane 
-def plot_endeff_yz(pmea, pref, label=None):
-    fig0, ax0 = plt.subplots(1, 1, figsize=(10.8,10.8))
+def plot_endeff_yz(fig0, ax0, pmea, pref, label=None):
     # Target 
-    ax0.plot(pref[:,1], pref[:,2], color='y', linewidth=4, linestyle='-', label='Reference', alpha=1.) 
+    ax0.plot(pref[:,1], pref[:,2], color='y', linewidth=LINEWIDTH, linestyle='-', label='Reference', alpha=1.) 
     # Measured
     if(label is None):
-        ax0.plot(pmea[:,1], pmea[:,2], color='b', linewidth=4, label='Measured', alpha=0.5)
+        ax0.plot(pmea[:,1], pmea[:,2], color='b', linewidth=LINEWIDTH, label='Measured', alpha=0.5)
     else: 
-        ax0.plot(pmea[:,1], pmea[:,2], color='b', linewidth=4, label=label, alpha=0.5)
+        ax0.plot(pmea[:,1], pmea[:,2], color='b', linewidth=LINEWIDTH, label=label, alpha=0.5)
     # Axis label & ticks
     ax0.set_ylabel('Z (m)', fontsize=26)
     ax0.set_xlabel('Y (m)', fontsize=26)
@@ -138,13 +143,11 @@ def plot_endeff_yz(pmea, pref, label=None):
     ax0.grid(True) 
     return fig0, ax0
 
-
  
 # Plot end-effector trajectory (y,z) plane 
-def plot_joint_traj(jmea, label):
-    fig0, ax0 = plt.subplots(1, 1, figsize=(19.2,10.8))
+def plot_joint_traj(fig0, ax0, jmea, label):
     # Measured
-    ax0.plot(xdata, jmea, color='b', linewidth=4, label=label, alpha=0.5) 
+    ax0.plot(xdata, jmea, color='b', linewidth=LINEWIDTH, label=label, alpha=0.5) 
     # Axis label & ticks
     ax0.set_ylabel('Joint position $q_1$ (rad)', fontsize=26)
     ax0.set_xlabel('Time (s)', fontsize=26)
@@ -183,8 +186,9 @@ if('circle_cssqp_joint' in PLOTS and 'circle_ssqp' in PLOTS):
     target_position[:,2] = rs[0].data['target_position_z'][N_START:N,0]
     p_mea1 = get_p_(r1.data['joint_positions'][N_START:N], pinrobot.model, pinrobot.model.getFrameId('contact'))
     p_mea2 = get_p_(r2.data['joint_positions'][N_START:N], pinrobot.model, pinrobot.model.getFrameId('contact'))
-    fig_circle, ax_circle = plot_endeff_yz(p_mea2, target_position, "Constrained") 
-    ax_circle.plot(p_mea1[:,1], p_mea1[:,2], color='g', linewidth=4, label='Unconstrained', alpha=0.5) 
+    fig_circle, ax_circle = plt.subplots(1, 1, figsize=(10.8,10.8)) 
+    plot_endeff_yz(fig_circle, ax_circle, p_mea2, target_position, "Constrained") 
+    ax_circle.plot(p_mea1[:,1], p_mea1[:,2], color='g', linewidth=LINEWIDTH, label='Unconstrained', alpha=0.5) 
     ax_circle.set_xlim(-0.33, +0.33)
     ax_circle.set_ylim(0.12, 0.8)
     ax_circle.plot(p_mea2[0,1], p_mea2[0,2], 'ro', markersize=16)
@@ -198,16 +202,17 @@ if('circle_cssqp_joint' in PLOTS and 'circle_ssqp' in PLOTS):
     jmea1 = r1.data['joint_positions'][N_START:N, 0]
     jmea2 = r2.data['joint_positions'][N_START:N, 0]
     jlb = [-0.05]*(N-N_START) ; jub = [0.05]*(N-N_START)
-    fig_q, ax_q = plot_joint_traj(jmea2, 'Constrained')
+    fig_q, ax_q = plt.subplots(1, 1, figsize=(19.2,10.8))
     # Constraint 
-    ax_q.plot(xdata, jlb, color='k', linewidth=4, linestyle='--', label='Constraint', alpha=0.6)
-    ax_q.plot(xdata, jub, color='k', linewidth=4, linestyle='--')
+    ax_q.plot(xdata, jlb, color='k', linewidth=LINEWIDTH, linestyle='--', label='Constraint', alpha=0.6)
+    ax_q.plot(xdata, jub, color='k', linewidth=LINEWIDTH, linestyle='--')
     MAX = 100
     ax_q.axhspan(jub[0], MAX, -MAX, MAX, color='gray', alpha=0.2, lw=0)      
-    ax_q.axhspan(-MAX, jlb[0], -MAX, MAX, color='gray', alpha=0.2, lw=0)      
+    ax_q.axhspan(-MAX, jlb[0], -MAX, MAX, color='gray', alpha=0.2, lw=0)    
+    plot_joint_traj(fig_q, ax_q, jmea2, 'Constrained')  
     ax_q.set_ylim(-0.75, 0.75)
     ax_q.set_xlim(0., (N-N_START)/config['ctrl_freq'])
-    ax_q.plot(xdata, jmea1, color='g', linewidth=4, label='Unconstrained', alpha=0.5) 
+    ax_q.plot(xdata, jmea1, color='g', linewidth=LINEWIDTH, label='Unconstrained', alpha=0.5) 
     handles, labels = ax_q.get_legend_handles_labels()
     fig_q.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.12, 0.885), prop={'size': 26}) 
     save_path = os.path.join(SAVE_PATH, 'circle_cssqp_joint_plot.pdf')
@@ -222,11 +227,12 @@ if('circle_cssqp_ee' in PLOTS):
     target_position[:,1] = rs[0].data['target_position_y'][N_START:N,0]
     target_position[:,2] = rs[0].data['target_position_z'][N_START:N,0]
     p_mea = get_p_(r3.data['joint_positions'][N_START:N], pinrobot.model, pinrobot.model.getFrameId('contact'))
-    fig, ax = plot_endeff_yz(p_mea, target_position) 
-    ax.axvline(0., color='k', linewidth=4, linestyle='--', label='Constraint', alpha=0.6)
+    fig, ax = plt.subplots(1, 1, figsize=(10.8,10.8)) 
+    ax.axvline(0., color='k', linewidth=LINEWIDTH, linestyle='--', label='Constraint', alpha=0.6)
     ax.set_xlim(-0.33, +0.33)
     ax.set_ylim(0.12, 0.8)
     ax.axhspan(0.8, -0.5, 0.5, -0., color='gray', alpha=0.2, lw=0)
+    plot_endeff_yz(fig, ax, p_mea, target_position) 
     handles, labels = ax.get_legend_handles_labels()
     fig.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.12, 0.885), prop={'size': 26}) 
     save_path = save_path = os.path.join(SAVE_PATH, 'circle_cssqp_ee_plot.pdf')
@@ -236,26 +242,28 @@ if('circle_cssqp_ee' in PLOTS):
 # Circle square shape
 if('square_cssqp' in PLOTS):
     target_position = np.zeros((N-N_START, 3)) 
-    target_position[:,0] = rs[0].data['target_position_x'][N_START:N,0]
-    target_position[:,1] = rs[0].data['target_position_y'][N_START:N,0]
-    target_position[:,2] = rs[0].data['target_position_z'][N_START:N,0]
+    target_position[:,0] = r4.data['target_position_x'][N_START:N,0]
+    target_position[:,1] = r4.data['target_position_y'][N_START:N,0]
+    target_position[:,2] = r4.data['target_position_z'][N_START:N,0]
     p_mea = get_p_(r4.data['joint_positions'][N_START:N], pinrobot.model, pinrobot.model.getFrameId('contact'))
-    fig, ax = plot_endeff_yz(p_mea, target_position) 
-    plb = r4.data['lb']
-    pub = r4.data['ub']
-    ax.axvline(plb[0,1], color='k', linewidth=4, linestyle='--', label='Constraint', alpha=0.6)
-    ax.axvline(pub[0,1], color='k', linewidth=4, linestyle='--', alpha=0.6)
-    ax.axhline(plb[0,2], color='k', linewidth=4, linestyle='--', alpha=0.6)
-    ax.axhline(pub[0,2], color='k', linewidth=4, linestyle='--', alpha=0.6)
-    MAX = 100
-    ax.axhspan(MAX, pub[0,2], 1., -0.5, color='gray', alpha=0.2, lw=0)       # up
-    ax.axhspan(plb[0,2], -MAX, 1., -0.5, color='gray', alpha=0.2, lw=0)       # down
-    ax.axhspan(MAX, -MAX, 0.5+pub[0,1], 1.1, color='gray', alpha=0.2, lw=0)  # right 
-    ax.axhspan(MAX, -MAX, -MAX, 0.5+plb[0,1], color='gray', alpha=0.2, lw=0) # left
-    ax.plot(p_mea[0,1], p_mea[0,2], 'ro', markersize=16)
-    # ax.text(0., 0.45, '$x_0$', fontdict={'size':26})
-    ax.set_xlim(-0.5, +0.5)
-    ax.set_ylim(0.18, 1.1)
+    fig, ax = plt.subplots(1, 1, figsize=(10.8,10.8)) 
+    
+    plb = r4.data['lb'][-1]
+    pub = r4.data['ub'][-1]
+    ax.axvline(plb[1], color='k', linewidth=LINEWIDTH, linestyle='--', label='Constraint', alpha=0.6)
+    ax.axvline(pub[1], color='k', linewidth=LINEWIDTH, linestyle='--', alpha=0.6)
+    ax.axhline(plb[2], color='k', linewidth=LINEWIDTH, linestyle='--', alpha=0.6)
+    ax.axhline(pub[2], color='k', linewidth=LINEWIDTH, linestyle='--', alpha=0.6)
+    MAX = 5
+    xmin = -0.47 ; xmax = +0.47
+    ymin = +0.10 ; ymax = +1.00
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+    ax.axhspan(pub[2], MAX, -MAX, MAX, color='gray', alpha=0.2, lw=0)      # up
+    ax.axhspan(-MAX, plb[2], -MAX, MAX, color='gray', alpha=0.2, lw=0)     # down
+    ax.axvspan(pub[1], MAX, -MAX, MAX, color='gray', alpha=0.2, lw=0)      # up
+    ax.axvspan(-MAX, plb[1], -MAX, MAX, color='gray', alpha=0.2, lw=0)     # down
+    plot_endeff_yz(fig, ax, p_mea, target_position) 
     handles, labels = ax.get_legend_handles_labels()
     fig.legend(handles, labels, loc='upper left', bbox_to_anchor=(0.12, 0.885), prop={'size': 26}) 
     save_path = save_path = os.path.join(SAVE_PATH, 'square_cssqp_plot.pdf')
