646874ab894cbcd5aa5cd23898a35228bf40942e
Sébastien Kleff <sk8001@nyu.edu>
2024-06-14 18:42:36 -0400

    debugging taichi bench

--- analysis/constrained/qp_benchmark.py
+++ analysis/constrained/qp_benchmark.py
@@ -130,7 +130,7 @@ def create_kuka_problem(x0):
     # Create the running models
     runningModels = []
     dt = 1e-2
-    T = 50
+    T = 40
     for t in range(T + 1):
         runningCostModel = crocoddyl.CostModelSum(state)
         # Add costs
@@ -196,23 +196,281 @@ def create_quadrotor_problem(x0):
     pb = crocoddyl.ShootingProblem(x0, [runningModel] * T, terminalModel)
     return pb 
 
+def create_humanoid_taichi_problem(target=np.array([0.4, 0, 1.2]), 
+                                   JOINT_CONSTRAINT=True, 
+                                   FORCE_COST=True,
+                                   FORCE_CONSTRAINT=True):
+    '''
+    Create shooting problem for Talos taichi task
+    '''
+    # Load robot
+    robot = example_robot_data.load("talos")
+    rmodel = robot.model
+    lims = rmodel.effortLimit
+    # lims[19:] *= 0.5  # reduced artificially the torque limits
+    rmodel.effortLimit = lims
+    # Create data structures
+    rdata = rmodel.createData()
+    state = crocoddyl.StateMultibody(rmodel)
+    actuation = crocoddyl.ActuationModelFloatingBase(state)
+    # Set integration time
+    DT = 5e-2
+    T = 10
+    # target = np.array([0.4, 0, 1.2])
+    # Initialize reference state, target and reference CoM
+    rightFoot = "right_sole_link"
+    leftFoot = "left_sole_link"
+    endEffector = "gripper_left_joint"
+    endEffectorId = rmodel.getFrameId(endEffector)
+    rightFootId = rmodel.getFrameId(rightFoot)
+    leftFootId = rmodel.getFrameId(leftFoot)
+    q0 = rmodel.referenceConfigurations["half_sitting"]
+    x0 = np.concatenate([q0, np.zeros(rmodel.nv)])
+    pin.forwardKinematics(rmodel, rdata, q0)
+    pin.updateFramePlacements(rmodel, rdata)
+    rfPos0 = rdata.oMf[rightFootId].translation
+    lfPos0 = rdata.oMf[leftFootId].translation
+    refGripper = rdata.oMf[rmodel.getFrameId("gripper_left_joint")].translation
+    comRef = (rfPos0 + lfPos0) / 2
+    comRef[2] = pin.centerOfMass(rmodel, rdata, q0)[2].item()
+    # Create two contact models used along the motion
+    contactModel1Foot = crocoddyl.ContactModelMultiple(state, actuation.nu)
+    contactModel2Feet = crocoddyl.ContactModelMultiple(state, actuation.nu)
+    supportContactModelLeft = crocoddyl.ContactModel6D(
+        state,
+        leftFootId,
+        pin.SE3.Identity(),
+        pin.LOCAL,
+        actuation.nu,
+        np.array([0, 40]),
+    )
+    supportContactModelRight = crocoddyl.ContactModel6D(
+        state,
+        rightFootId,
+        pin.SE3.Identity(),
+        pin.LOCAL,
+        actuation.nu,
+        np.array([0, 40]),
+    )
+    contactModel1Foot.addContact(rightFoot + "_contact", supportContactModelRight)
+    contactModel2Feet.addContact(leftFoot + "_contact", supportContactModelLeft)
+    contactModel2Feet.addContact(rightFoot + "_contact", supportContactModelRight)
+    # Cost for self-collision
+    maxfloat = np.inf #sys.float_info.max !!! needs to be inf otherwise HPIPM would crash
+    xlb = np.concatenate(
+        [
+            -maxfloat * np.ones(6),  # dimension of the SE(3) manifold
+            rmodel.lowerPositionLimit[7:],
+            -maxfloat * np.ones(state.nv),
+        ]
+    )
+    xub = np.concatenate(
+        [
+            maxfloat * np.ones(6),  # dimension of the SE(3) manifold
+            rmodel.upperPositionLimit[7:],
+            maxfloat * np.ones(state.nv),
+        ]
+    )
+    bounds = crocoddyl.ActivationBounds(xlb, xub, 1.0)
+    xLimitResidual = crocoddyl.ResidualModelState(state, x0, actuation.nu)
+    xLimitActivation = crocoddyl.ActivationModelQuadraticBarrier(bounds)
+    limitCost = crocoddyl.CostModelResidual(state, xLimitActivation, xLimitResidual)
+    # Cost for state and control
+    xResidual = crocoddyl.ResidualModelState(state, x0, actuation.nu)
+    xActivation = crocoddyl.ActivationModelWeightedQuad(
+        np.array([0] * 3 + [10.0] * 3 + [0.01] * (state.nv - 6) + [10] * state.nv) ** 2
+    )
+    uResidual = crocoddyl.ResidualModelControl(state, actuation.nu)
+    xTActivation = crocoddyl.ActivationModelWeightedQuad(
+        np.array([0] * 3 + [10.0] * 3 + [0.01] * (state.nv - 6) + [100] * state.nv) ** 2
+    )
+    xRegCost = crocoddyl.CostModelResidual(state, xActivation, xResidual)
+    uRegCost = crocoddyl.CostModelResidual(state, uResidual)
+    xRegTermCost = crocoddyl.CostModelResidual(state, xTActivation, xResidual)
+
+    # Cost for target reaching: hand and foot
+    handTrackingResidual = crocoddyl.ResidualModelFramePlacement(
+        state, endEffectorId, pin.SE3(np.eye(3), target), actuation.nu
+    )
+    handTrackingActivation = crocoddyl.ActivationModelWeightedQuad(
+        np.array([1] * 3 + [0.0001] * 3) ** 2
+    )
+    handTrackingCost = crocoddyl.CostModelResidual(
+        state, handTrackingActivation, handTrackingResidual
+    )
+
+    footTrackingResidual = crocoddyl.ResidualModelFramePlacement(
+        state, leftFootId, pin.SE3(np.eye(3), np.array([0.0, 0.4, 0.0])), actuation.nu
+    )
+    footTrackingActivation = crocoddyl.ActivationModelWeightedQuad(
+        np.array([1, 1, 0.1] + [1.0] * 3) ** 2
+    )
+    footTrackingCost1 = crocoddyl.CostModelResidual(
+        state, footTrackingActivation, footTrackingResidual
+    )
+    footTrackingResidual = crocoddyl.ResidualModelFramePlacement(
+        state,
+        leftFootId,
+        pin.SE3(np.eye(3), np.array([0.3, 0.15, 0.35])),
+        actuation.nu,
+    )
+    footTrackingCost2 = crocoddyl.CostModelResidual(
+        state, footTrackingActivation, footTrackingResidual
+    )
+    # Cost for CoM reference
+    # comResidual = crocoddyl.ResidualModelCoMPosition(state, comRef, actuation.nu)
+    # comTrack = crocoddyl.CostModelResidual(state, comResidual)
+    # Create cost model per each action model. We divide the motion in 3 phases plus its
+    # terminal model.
+    runningCostModel1 = crocoddyl.CostModelSum(state, actuation.nu)
+    runningCostModel2 = crocoddyl.CostModelSum(state, actuation.nu)
+    runningCostModel3 = crocoddyl.CostModelSum(state, actuation.nu)
+    terminalCostModel = crocoddyl.CostModelSum(state, actuation.nu)
+    # Then let's added the running and terminal cost functions
+    JOINT_CONSTRAINT = True
+    runningCostModel1.addCost("gripperPose", handTrackingCost, 1e2)
+    runningCostModel1.addCost("stateReg", xRegCost, 1e-3)
+    runningCostModel1.addCost("ctrlReg", uRegCost, 1e-4)
+    if not JOINT_CONSTRAINT:
+        runningCostModel1.addCost("limitCost", limitCost, 1e3)
+    runningCostModel2.addCost("gripperPose", handTrackingCost, 1e2)
+    runningCostModel2.addCost("footPose", footTrackingCost1, 1e1)
+    runningCostModel2.addCost("stateReg", xRegCost, 1e-3)
+    runningCostModel2.addCost("ctrlReg", uRegCost, 1e-4)
+    if not JOINT_CONSTRAINT:
+        runningCostModel2.addCost("limitCost", limitCost, 1e3)
+    runningCostModel3.addCost("gripperPose", handTrackingCost, 1e2)
+    runningCostModel3.addCost("footPose", footTrackingCost2, 1e1)
+    runningCostModel3.addCost("stateReg", xRegCost, 1e-3)
+    runningCostModel3.addCost("ctrlReg", uRegCost, 1e-4)
+    if not JOINT_CONSTRAINT:
+        runningCostModel3.addCost("limitCost", limitCost, 1e3)
+    terminalCostModel.addCost("gripperPose", handTrackingCost, 1e2)
+    terminalCostModel.addCost("stateReg", xRegTermCost, 1e-3)
+    if not JOINT_CONSTRAINT:
+        terminalCostModel.addCost("limitCost", limitCost, 1e3)
+    constraintModelManager = crocoddyl.ConstraintModelManager(state, actuation.nu)
+    FORCE_COST       = True
+    FORCE_CONSTRAINT = True
+    fref = pin.Force.Zero()
+    ForceResidual = crocoddyl.ResidualModelContactForce(state, rightFootId, fref, 6, actuation.nu)
+    if FORCE_COST:
+        Forcecost = crocoddyl.CostModelResidual(state, ForceResidual)
+        runningCostModel1.addCost("forcecost1", Forcecost, 1e-3)
+        runningCostModel2.addCost("forcecost2", Forcecost, 1e-3)
+        runningCostModel3.addCost("forcecost3", Forcecost, 1e-3)
+    if FORCE_CONSTRAINT:
+        constraintForce = crocoddyl.ConstraintModelResidual(state, ForceResidual, np.array([0., 0, 0]*2), np.array([np.inf, np.inf, np.inf]*2))
+        constraintModelManager.addConstraint("force", constraintForce)
+    if JOINT_CONSTRAINT:
+        constraintState = crocoddyl.ConstraintModelResidual(state, xLimitResidual, xlb, xub)
+        constraintModelManager.addConstraint("state", constraintState)
+    # Create the action model
+    dmodelRunning1 = crocoddyl.DifferentialActionModelContactFwdDynamics(
+        state, actuation, contactModel2Feet, runningCostModel1, constraintModelManager, 0., True)
+    dmodelRunning2 = crocoddyl.DifferentialActionModelContactFwdDynamics(
+        state, actuation, contactModel1Foot, runningCostModel2, constraintModelManager, 0., True)
+    dmodelRunning3 = crocoddyl.DifferentialActionModelContactFwdDynamics(
+        state, actuation, contactModel1Foot, runningCostModel3, constraintModelManager, 0., True)
+    dmodelTerminal = crocoddyl.DifferentialActionModelContactFwdDynamics(
+        state, actuation, contactModel1Foot, terminalCostModel
+    )
+    runningModel1 = crocoddyl.IntegratedActionModelEuler(dmodelRunning1, DT)
+    runningModel2 = crocoddyl.IntegratedActionModelEuler(dmodelRunning2, DT)
+    runningModel3 = crocoddyl.IntegratedActionModelEuler(dmodelRunning3, DT)
+    terminalModel = crocoddyl.IntegratedActionModelEuler(dmodelTerminal, 0)
+    # Problem definition
+    x0 = np.concatenate([q0, pin.utils.zero(state.nv)])
+    pb = crocoddyl.ShootingProblem(
+        x0, [runningModel1] * T + [runningModel2] * T + [runningModel3] * T, terminalModel
+    )
+    return pb
+
+pb = create_humanoid_taichi_problem()
+N_ocp = pb.T
+xs_init = [pb.x0] * (N_ocp + 1)
+robot = example_robot_data.load("talos")
+rmodel = robot.model
+state = crocoddyl.StateMultibody(rmodel)
+actuation = crocoddyl.ActuationModelFloatingBase(state)
+us_init = [np.zeros( actuation.nu)] * N_ocp
+# Define solver
+ddp0 = mim_solvers.SolverCSQP(pb) #CSQP(problem, "StagewiseQP")
+ddp0.with_qp_callbacks = True 
+ddp1 = CSQP(pb, "ProxQP")
+ddp2 = CSQP(pb, "OSQP")
+ddp4 = CSQP(pb, "HPIPM_DENSE")
+ddp5 = CSQP(pb, "HPIPM_OCP")
+ddp0.with_callbacks = True
+ddp1.with_callbacks = True
+ddp2.with_callbacks = True
+ddp4.with_callbacks = True
+ddp5.with_callbacks = True
+max_qp_iters = 10000
+ddp0.max_qp_iters = max_qp_iters
+ddp1.max_qp_iters = max_qp_iters
+ddp2.max_qp_iters = max_qp_iters
+ddp4.max_qp_iters = max_qp_iters
+ddp5.max_qp_iters = max_qp_iters
+eps_abs = 1e-3  
+eps_rel = 0.
+ddp0.eps_abs = eps_abs
+ddp0.eps_rel = eps_rel
+ddp1.eps_abs = eps_abs
+ddp1.eps_rel = eps_rel
+ddp2.eps_abs = eps_abs
+ddp2.eps_rel = eps_rel
+ddp4.eps_abs = eps_abs
+ddp4.eps_rel = eps_rel
+ddp5.eps_rel = eps_rel
+ddp0.equality_qp_initial_guess = False
+ddp1.equality_qp_initial_guess = False
+ddp2.equality_qp_initial_guess = False
+ddp4.equality_qp_initial_guess = False
+ddp5.equality_qp_initial_guess = False
+ddp0.update_rho_with_heuristic = True
+import time 
+# Stagewise QP
+print("\n ------ STAGEWISE (computeDirection)------ ")
+converged = ddp0.solve(xs_init, us_init, 0)
+t0 = time.time()
+ddp0.computeDirection(True)
+print("Stagewise : ", time.time() - t0)
+print("------------------------ \n")
+# # ProxQP
+# print("\n ------ ProxQP ------ ")
+# converged = ddp1.solve(xs_init, us_init, 1)
+# print("------------------------ \n")
+# # OSQP
+# print("\n ------ OSQP ------ ")
+# converged = ddp2.solve(xs_init, us_init, 1)
+# print("------------------------ \n")
+# # HPIPM
+# print("\n ------ HPIPM DENSE ------ ")
+# converged = ddp4.solve(xs_init, us_init, 1)
+# print("------------------------ \n")
+# # HPIPM
+# print("\n ------ HPIPM OCP ------ ")
+# converged = ddp5.solve(xs_init, us_init, 1)
+# print("------------------------ \n")
 
 # Solver params
 MAXITER     = 1     
 TOL         = 1e-4 
 CALLBACKS   = False
-MAX_QP_ITER = 1000
+MAX_QP_ITER = 10000
 MAX_QP_TIME = int(0.5*1e3) # in ms
-EPS_ABS     = 1e-5
+EPS_ABS     = 1e-3
 EPS_REL     = 0.
 SAVE        = False # Save figure 
 
 # Benchmark params
 SEED = 1 ; np.random.seed(SEED)
-N_samples = 100
+N_samples = 2
 names = [
     #    'Pendulum'] # maxiter = 500
-         'Kuka'] # maxiter = 100
+        #  'Kuka'] # maxiter = 100
+         'Taichi'] #
         # #  'Cartpole']  #--> need to explain why it doesn't converge otherwise leave it out 
         #  'Quadrotor'] # maxiter = 200
 
@@ -230,6 +488,7 @@ cartpole_x0  = np.array([0., 3.14, 0., 0.])
 kuka_x0      = np.array([0.1, 0.2, 0., 0., -0.2, 0.2] + [0.]*6)
 quadrotor    = example_robot_data.load('hector') 
 quadrotor_x0 = np.array(list(quadrotor.q0) + [0.]*quadrotor.model.nv) 
+taichi_p0    = np.array([0.4, 0, 1.2])
 
 # Create 1 solver of each type for each problem
 print('------')
@@ -242,6 +501,8 @@ for k,name in enumerate(names):
         pb = create_kuka_problem(kuka_x0) 
     if(name == "Quadrotor"): 
         pb = create_quadrotor_problem(quadrotor_x0) 
+    if(name == "Taichi"): 
+        pb = create_humanoid_taichi_problem(taichi_p0) 
 
     # Create solver CSQP 
     solvercsqp = mim_solvers.SolverCSQP(pb)
@@ -298,13 +559,17 @@ cartpole_x0_samples  = np.zeros((N_samples, 4))
 kuka                 = load_pinocchio_wrapper("iiwa", locked_joints=["A7"])
 kuka_x0_samples      = np.zeros((N_samples, kuka.model.nq + kuka.model.nv))
 quadrotor            = example_robot_data.load('hector') 
-humanoid             = example_robot_data.load('talos')
 quadrotor_x0_samples = np.zeros((N_samples, quadrotor.model.nq + quadrotor.model.nv))
+taichi_p0_samples  = np.zeros((N_samples, 3))
 for i in range(N_samples):
     pendulum_x0_samples[i,:]  = np.array([np.pi*(2*np.random.rand()-1), 0., 0., 0.])
     cartpole_x0_samples[i,:]  = np.array([0., np.pi/2, 0., 0.])
     kuka_x0_samples[i,:]      = np.concatenate([pin.randomConfiguration(kuka.model), np.zeros(kuka.model.nv)])
     quadrotor_x0_samples[i,:] = np.concatenate([pin.randomConfiguration(quadrotor.model), np.zeros(quadrotor.model.nv)])
+    quadrotor_x0_samples[i,:] = np.concatenate([pin.randomConfiguration(quadrotor.model), np.zeros(quadrotor.model.nv)])
+    err = np.zeros(3)
+    err[2] = 2*np.random.rand(1) - 1
+    taichi_p0_samples[i,:]  = taichi_p0 #+ 0.05*err
 
 print("Created "+str(N_samples)+" random initial states per model !")
 
@@ -350,13 +615,19 @@ for i in range(N_samples):
         if(name == "Cartpole"):  x0 = cartpole_x0_samples[i,:]
         if(name == "Kuka"):      x0 = kuka_x0_samples[i,:]
         if(name == "Quadrotor"): x0 = quadrotor_x0_samples[i,:]
+        if(name == "Taichi"):    p0 = taichi_p0_samples[i,:]
 
         # CSQP
         print("   Problem : "+name+" CSQP")
         solvercsqp = solversCSQP[k]
-        solvercsqp.problem.x0 = x0
-        solvercsqp.xs = [x0] * (solvercsqp.problem.T + 1) 
-        solvercsqp.us = solvercsqp.problem.quasiStatic([x0] * solvercsqp.problem.T)
+        if(name == "Taichi"):
+            models = list(solvercsqp.problem.runningModels) + [solvercsqp.problem.terminalModel]
+            for m in models: m.differential.costs.costs["gripperPose"].cost.residual.reference = pin.SE3(np.eye(3), p0.copy())
+        else:
+            solvercsqp.problem.x0 = x0
+        solvercsqp.xs = [solvercsqp.problem.x0] * (solvercsqp.problem.T + 1) 
+        solvercsqp.us = solvercsqp.problem.quasiStatic([solvercsqp.problem.x0] * solvercsqp.problem.T)
+        solvercsqp.with_qp_callbacks = True
         solvercsqp.solve(solvercsqp.xs, solvercsqp.us, 0, False)
         t1 = time.time()
         solvercsqp.computeDirection(True)
@@ -376,9 +647,13 @@ for i in range(N_samples):
         # OSQP
         print("   Problem : "+name+" OSQP")
         solverosqp = solversOSQP[k]
-        solverosqp.problem.x0 = x0
-        solverosqp.xs = [x0] * (solverosqp.problem.T + 1) 
-        solverosqp.us = solverosqp.problem.quasiStatic([x0] * solverosqp.problem.T)
+        if(name == "Taichi"):
+            models = list(solvercsqp.problem.runningModels) + [solvercsqp.problem.terminalModel]
+            for m in models: m.differential.costs.costs["gripperPose"].cost.residual.reference = pin.SE3(np.eye(3), p0.copy())
+        else:
+            solvercsqp.problem.x0 = x0
+        solverosqp.xs = [solverosqp.problem.x0] * (solverosqp.problem.T + 1) 
+        solverosqp.us = solverosqp.problem.quasiStatic([solverosqp.problem.x0] * solverosqp.problem.T)
         solverosqp.solve(solverosqp.xs, solverosqp.us, MAXITER, False)
         if(solverosqp.found_qp_sol):
             solved = (solverosqp.norm_primal < EPS_ABS and solverosqp.norm_dual < EPS_ABS and solverosqp.qp_iters <= MAX_QP_ITER)
@@ -397,9 +672,13 @@ for i in range(N_samples):
         # HPIPM_DENSE
         print("   Problem : "+name+" HPIPM_DENSE")
         solverhpipm_dense = solversHPIPM_dense[k]
-        solverhpipm_dense.problem.x0 = x0
-        solverhpipm_dense.xs = [x0] * (solverhpipm_dense.problem.T + 1) 
-        solverhpipm_dense.us = solverhpipm_dense.problem.quasiStatic([x0] * solverhpipm_dense.problem.T)
+        if(name == "Taichi"):
+            models = list(solvercsqp.problem.runningModels) + [solvercsqp.problem.terminalModel]
+            for m in models: m.differential.costs.costs["gripperPose"].cost.residual.reference = pin.SE3(np.eye(3), p0.copy())
+        else:
+            solvercsqp.problem.x0 = x0
+        solverhpipm_dense.xs = [solverhpipm_dense.problem.x0] * (solverhpipm_dense.problem.T + 1) 
+        solverhpipm_dense.us = solverhpipm_dense.problem.quasiStatic([solverhpipm_dense.problem.x0] * solverhpipm_dense.problem.T)
         solverhpipm_dense.solve(solverhpipm_dense.xs, solverhpipm_dense.us, MAXITER, False)
         if(solverhpipm_dense.found_qp_sol):
             solved = (solverhpipm_dense.norm_primal < EPS_ABS and solverhpipm_dense.norm_dual < EPS_ABS and solverhpipm_dense.qp_iters <= MAX_QP_ITER)
@@ -418,9 +697,13 @@ for i in range(N_samples):
         # HPIPM_OCP        
         print("   Problem : "+name+" HPIPM_OCP")
         solverhpipm_ocp = solversHPIPM_ocp[k]
-        solverhpipm_ocp.problem.x0 = x0
-        solverhpipm_ocp.xs = [x0] * (solverhpipm_ocp.problem.T + 1) 
-        solverhpipm_ocp.us = solverhpipm_ocp.problem.quasiStatic([x0] * solverhpipm_ocp.problem.T)
+        if(name == "Taichi"):
+            models = list(solvercsqp.problem.runningModels) + [solvercsqp.problem.terminalModel]
+            for m in models: m.differential.costs.costs["gripperPose"].cost.residual.reference = pin.SE3(np.eye(3), p0.copy())
+        else:
+            solvercsqp.problem.x0 = x0
+        solverhpipm_ocp.xs = [solverhpipm_ocp.problem.x0] * (solverhpipm_ocp.problem.T + 1) 
+        solverhpipm_ocp.us = solverhpipm_ocp.problem.quasiStatic([solverhpipm_ocp.problem.x0] * solverhpipm_ocp.problem.T)
         # import pdb ; pdb.set_trace()
         solverhpipm_ocp.solve(solverhpipm_ocp.xs, solverhpipm_ocp.us, MAXITER, False)
             # Check convergence
